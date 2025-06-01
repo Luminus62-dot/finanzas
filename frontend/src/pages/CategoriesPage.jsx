641dc9745e9989381376508490a5d20240d11b08
Luminus62-dot <lzaldivar157@gmail.com>
2025-06-01 12:49:21 -0700

    Update CategoriesPage.jsx

--- frontend/src/pages/CategoriesPage.jsx
+++ frontend/src/pages/CategoriesPage.jsx
@@ -30,7 +30,7 @@ const CategoriesPage = ({ token }) => {
         toast.error("Token no encontrado. Por favor, inicia sesión de nuevo."); // <-- USANDO TOAST
         return;
       }
-      const res = await axios.get("https://mi-dinero-hoy.onrender.com/api/categories");
+      const res = await axios.get("REACT_APP_BACKEND_URL=https://mi-dinero-hoy.onrender.com/api/categories");
       setCategories(res.data);
       // setMessage(''); // <-- ELIMINADO
     } catch (err) {
@@ -54,7 +54,7 @@ const CategoriesPage = ({ token }) => {
   const handleAddCategory = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("https://mi-dinero-hoy.onrender.com/api/categories", {
+      await axios.post("REACT_APP_BACKEND_URL=https://mi-dinero-hoy.onrender.com/api/categories", {
         name: newCategoryName,
         type: newCategoryType,
       });
@@ -86,7 +86,7 @@ const CategoriesPage = ({ token }) => {
 
     try {
       await axios.put(
-        `https://mi-dinero-hoy.onrender.com/api/categories/${editingCategory._id}`,
+        `REACT_APP_BACKEND_URL=https://mi-dinero-hoy.onrender.com/api/categories/${editingCategory._id}`,
         {
           name: editingCategory.name,
           type: editingCategory.type,
@@ -106,7 +106,7 @@ const CategoriesPage = ({ token }) => {
   const handleDeleteCategory = async (id) => {
     if (window.confirm("Are you sure you want to delete this category?")) {
       try {
-        await axios.delete(`https://mi-dinero-hoy.onrender.com/api/categories/${id}`);
+        await axios.delete(`REACT_APP_BACKEND_URL=https://mi-dinero-hoy.onrender.com/api/categories/${id}`);
         toast.success("Category deleted successfully!"); // <-- USANDO TOAST
         fetchCategories(); // Refresh categories list
       } catch (err) {
